d19ac7820ac3cd7cf52246e615e5d812d685e1ee
Madhu Chavva <madhu.chavva@gmail.com>
2025-05-21 13:13:19 -0700

    show pre-order modal

--- components/Action.js
+++ components/Action.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import Typing from './Typing';
+import PreOrderModal from './PreOrderModal';
 
 export default function Action() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div className="max-w-full md:mx-10 lg:mx-20 xl:mx-auto">
       <div
@@ -18,12 +25,12 @@ export default function Action() {
           🔥 Your Gut Called. It Wants ENLITE.
         </h2>
         <div className="flex flex-wrap justify-center mb-10 -mt-4 space-y-4 pace-x-4">
-          <a
+          <button
             className="md:mt-2 text-center text-gray-100 text-md md:text-xl font-semibold px-5 py-3 ease-in-out transform text-sm transition border-2 duration-400 hover:-translate-y-0.5 hover:text-gray-300 hover:border-gray-300 rounded-xl"
-            href="#"
+            onClick={openModal}
           >
             Pre-Order Now!
-          </a>
+          </button>
         </div>
         <div className="flex -ml-4 space-x-4 text-center text-gray-100 md:justify-center md:space-x-8">
           <div className="ml-4">
@@ -31,6 +38,9 @@ export default function Action() {
           </div>
         </div>
       </div>
+
+      {/* Pre-Order Modal */}
+      <PreOrderModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 }
